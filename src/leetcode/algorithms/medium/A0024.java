21c1cb656f6d78d0f5c40c6fe485f043e614bfd4
XuShicheng <602300728@qq.com>
2020-02-11 09:47:11 +0800

    20200211

---
+++ src/leetcode/algorithms/medium/A0024.java
@@ -0,0 +1,43 @@
+package leetcode.algorithms.medium;
+
+/**
+ * 24. 两两交换链表中的节点
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * 
+ * 示例:
+ * 给定 1->2->3->4, 你应该返回 2->1->4->3.
+ *
+ */
+
+public class A0024 {
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	public ListNode swapPairs(ListNode head) {
+		ListNode res = new ListNode(0);
+		res.next = head;
+		ListNode p = res, q;
+		while (p.next != null && p.next.next != null) {
+			q = p.next;
+			p.next = q.next;
+			q.next = q.next.next;
+			p.next.next = q;
+			p = q;
+		}
+		return res.next;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/leetcode/algorithms/medium/A0029.java
@@ -0,0 +1,55 @@
+package leetcode.algorithms.medium;
+
+/**
+ * 29. 两数相除
+ * 给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
+ * 返回被除数 dividend 除以除数 divisor 得到的商。
+ * 
+ * 示例 1:
+ * 输入: dividend = 10, divisor = 3
+ * 输出: 3
+ * 示例 2:
+ * 输入: dividend = 7, divisor = -3
+ * 输出: -2
+ * 说明:
+ * 被除数和除数均为 32 位有符号整数。
+ * 除数不为 0。
+ * 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−2^31,  2^31 − 1]。本题中，如果除法结果溢出，则返回 2^31 − 1。
+ *
+ * 思路：位移法。左移1位相当于*2，右移1位相当于/2.
+ */
+
+public class A0029 {
+
+	public int divide(int dividend, int divisor) {
+		boolean sign = (dividend > 0) ^ (divisor > 0);
+		int result = 0;
+		if (dividend > 0)
+			dividend = -dividend;
+		if (divisor > 0)
+			divisor = -divisor;
+		while (dividend <= divisor) {
+			int temp_result = -1;
+			int temp_divisor = divisor;
+			while (dividend <= (temp_divisor << 1)) {
+				if (temp_divisor <= (Integer.MIN_VALUE >> 1))
+					break;
+				temp_result = temp_result << 1;
+				temp_divisor = temp_divisor << 1;
+			}
+			dividend = dividend - temp_divisor;
+			result += temp_result;
+		}
+		if (!sign) {
+			if (result <= Integer.MIN_VALUE)
+				return Integer.MAX_VALUE;
+			result = -result;
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new A0029().divide(10, 3));
+	}
+
+}
